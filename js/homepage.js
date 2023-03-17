8ac2794b437ef320faca7e5cd17a6bc81e88edd4
nehCnitsuJ <s10222137@connect.np.edu.sg>
2023-03-17 17:13:11 +0800

    REVAMPED EVEYRHTING

--- js/homepage.js
+++ js/homepage.js
@@ -2,7 +2,6 @@
 const dropdownBtn = document.getElementById("dropdown-btn");
 const navBar = document.getElementById("navbar");
 const dropdown = document.querySelector(".dropdown");
-const sticky = navBar.offsetTop;
 //Offsets when navbar activates
 const offset = 50;
 
@@ -11,15 +10,22 @@ dropdownBtn.addEventListener("click", () => {
   dropdown.classList.toggle("dropdown-open");
 });
 
-// When the user scrolls the page, execute sticky navbar
+// When the user scrolls the page, execute myFunction
 window.onscroll = function () {
-  sticknavbar();
+  stickynav();
 };
 
-function sticknavbar() {
+// Get the navbar
+var navbar = document.getElementById("navbaractual");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickynav() {
   if (window.pageYOffset >= sticky) {
-    navBar.classList.add("sticky");
+    navbar.classList.add("sticky");
   } else {
-    navBar.classList.remove("sticky");
+    navbar.classList.remove("sticky");
   }
 }
